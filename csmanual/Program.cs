e2a6507a80a22bb283c21264baae9165c71f5601
Jan Kotas <jkotas@microsoft.com>
2021-10-03 19:30:56 -0700

    Implement shared helpers

--- csmanual/Program.cs
+++ csmanual/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 // Handwritten example that demonstrates performance that cswinrt can achieve with efficient bindings
 
@@ -25,27 +28,86 @@ namespace WinRT
 {
     public static class ExceptionHelpers
     {
+        // .NET 6: [StackTraceHidden]
         public static void ThrowExceptionForHR(int hr)
         {
-            // TODO
+            if (hr < 0)
+                Throw(hr);
+
+            // .NET 6: [StackTraceHidden]
+            static void Throw(int hr)
+            {
+                Exception ex = GetExceptionForHR(hr, useGlobalErrorState: true, out bool restoredExceptionFromGlobalState);
+                if (restoredExceptionFromGlobalState)
+                {
+                    ExceptionDispatchInfo.Capture(ex).Throw();
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        // Placeholder implemenentation. See https://github.com/microsoft/CsWinRT/blob/master/src/WinRT.Runtime/ExceptionHelpers.cs
+        // for actual implementation.
+        private static Exception GetExceptionForHR(int hr, bool useGlobalErrorState, out bool restoredExceptionFromGlobalState)
+        {
+            restoredExceptionFromGlobalState = false;
+            return new Exception() { HResult = hr };
         }
     }
 
     public class Factory
     {
+        private string _name;
+        private Guid _iid;
+        private IntPtr _thisPtr;
+
         public Factory(string name, Guid iid)
         {
-            // TODO
+            _name = name;
+            _iid = iid;
         }
 
         public IntPtr ThisPtr
         {
             get
             {
-                // TODO
-                return IntPtr.Zero;
+                return (_thisPtr != IntPtr.Zero) ? _thisPtr : InitializeThisPtr();
             }
         }
+
+        private IntPtr InitializeThisPtr()
+        {
+            IntPtr hstring = IntPtr.Zero;
+            IntPtr thisPtr = IntPtr.Zero;
+            try
+            {
+                WinRT.ExceptionHelpers.ThrowExceptionForHR(WindowsCreateString(_name, _name.Length, out hstring));
+                WinRT.ExceptionHelpers.ThrowExceptionForHR(RoGetActivationFactory(hstring, ref _iid, out thisPtr));
+            }
+            finally
+            {
+                WindowsDeleteString(hstring);
+            }
+
+            // TODO: Deal with non-agile factories and other rare cases.
+
+            if (Interlocked.CompareExchange(ref _thisPtr, thisPtr, IntPtr.Zero) != IntPtr.Zero)
+                Marshal.Release(thisPtr);
+
+            return _thisPtr;
+        }
+
+        [DllImport("api-ms-win-core-winrt-l1-1-0.dll")]
+        private static extern unsafe int RoGetActivationFactory(IntPtr activatableClassId, ref Guid iid, out IntPtr factory);
+
+        [DllImport("api-ms-win-core-winrt-string-l1-1-0.dll", CharSet = CharSet.Unicode)]
+        private static extern unsafe int WindowsCreateString(string sourceString, int length, out IntPtr hstring);
+
+        [DllImport("api-ms-win-core-winrt-string-l1-1-0.dll")]
+        internal static extern int WindowsDeleteString(IntPtr hstring);
     }
 }
 
